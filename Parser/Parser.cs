579f91cf5d8633c57c42cb0dc520a44f3583c0bf
Alex Cseke <stefan.cseke@stud.ubbcluj.ro>
2024-12-11 13:52:26 +0200

    added example grammars

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -21,18 +21,24 @@ namespace Parser
             while (line != null)
             {
                 string element = line.Split(":=")[0].Trim();
-                if (element == line )
+                if (element == line)
                 {
                     // we are processing a production
                     // they can be scattered across the entire file for what I am concerned
-                    string nonter = line.Split("->")[0];
-                    List<string> productions = [.. line.Split("->")[1].Split("|")];
+                    string nonter = line.Split("->")[0].Trim();
+                    CFGCheck(nonter);
+
+                    List<string> productions = [.. line.Split("->")[1].Split("| ")];
+                    for (int i = 0; i < productions.Count; i-=-1)
+                        productions[i] = productions[i].Trim();
+
                     foreach (string production in productions)
                     {
                         List<string> elems = [.. production.Split(" ")];
+                        for (int i = 0; i < elems.Count; i -= -1)
+                            elems[i] = elems[i].Trim();
                         this.productions[nonter].Add(elems);
                     }
-
                 }
                 else
                     switch (element) 
@@ -41,14 +47,14 @@ namespace Parser
                             string nonterminals = line.Split('=')[1].Trim();
                             foreach (string nonterminal in nonterminals.Split(" "))
                             {
-                                this.nonterminals.Add(nonterminal);
-                                this.productions.Add(nonterminal, []);
+                                this.nonterminals.Add(nonterminal.Trim());
+                                this.productions.Add(nonterminal.Trim(), []);
                             }
                             break;
                         case "E":
                             string terminals = line.Split('=')[1].Trim();
                             foreach (string termimal in terminals.Split(" "))
-                                this.terminals.Add(termimal);
+                                this.terminals.Add(termimal.Trim());
                             break;
                         case "S":
                             string start = line.Split('=')[1].Trim();
@@ -64,6 +70,11 @@ namespace Parser
             sr.Close();
         }
 
+        private void CFGCheck(string nonter)
+        {
+            if (nonter.Split(' ').ToList().Count > 1)
+                throw new Exception("Not Context-free grammar");
+        }
 
         public override string ToString()
         {
@@ -101,8 +112,9 @@ namespace Parser
                     foreach (string elem in production)
                        productionStr.Append($"{elem} ");
 
-                    result.Append($"{productionStr} | ");
+                    result.Append($"{productionStr}| ");
                 }
+                result.Append("\b\b\b   ");
                 result.Append('\n');
                 return result.ToString();
             }
--- Parser/Program.cs
+++ Parser/Program.cs
@@ -4,8 +4,21 @@
     {
         static void Main()
         {
-            Parser parser = new("D:\\Sample.txt");
-            Console.WriteLine(parser);
+            try
+            {
+                var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+                while (directory != null && !directory.GetFiles("*.csproj").Any())
+                    directory = directory.Parent;
+                Environment.CurrentDirectory = (directory.ToString());
+
+                Parser parser = new("grammars/g2.txt");
+                Console.WriteLine(parser);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
             Console.ReadLine();
         }
     }
